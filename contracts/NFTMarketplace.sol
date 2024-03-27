c009169e096e8f5b803e3a3d8bb00b61ee8de37f
snsakib <hello@snsakib.com>
2024-03-27 23:12:21 +0600

    version changes

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,12 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _nftIds;
+    uint256 private _nftIds;
     address payable contractOwner;
     uint256 public listingPrice = 10000000 wei;
 
